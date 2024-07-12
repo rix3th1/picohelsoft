a29ebae8466505573f32480404973ca1fa265595
Richard A. Rich <rojasricor@gmail.com>
2024-07-12 13:01:34 -0500

    refactor: Update DashboardMainAside.vue styles for responsiveness

--- components/dashboard/DashboardMainAside.vue
+++ components/dashboard/DashboardMainAside.vue
@@ -1,6 +1,6 @@
 <template>
   <aside
-    class="flex flex-col h-[calc(100vh-5rem)] w-80 py-5 bg-neutral-200/30 dark:bg-neutral-500/20 border-r border-neutral-300/50 dark:border-neutral-700/50"
+    class="flex flex-col h-[calc(100vh-5rem)] py-1 w-80 bg-neutral-200/30 dark:bg-neutral-500/20 border-r border-neutral-300/50 dark:border-neutral-700/50"
   >
     <DashboardTopAside />
     <DashboardBottomAside />
